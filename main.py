e4b9143e1a76451a8dfc1766e8d9f840d670f92f
unknown <sergey.avalon01@gmail.com>
2024-09-16 18:24:41 +0300

    создал игру с ботом

---
+++ main.py
@@ -0,0 +1,25 @@
+import random
+
+points = {
+    "comp": 0,
+    "player": 0
+}
+
+while True:
+    a = random.randint(10, 90)
+    b = random.randint(10, 90)
+    op = random.choice(("+", "-"))
+    if op == "+":
+        ans = a + b
+        user = input(f"{a} + {b} = ")
+    else:
+        ans = a - b
+        user = input(f"{a} - {b} = ")
+    if user == str(ans):
+        print('верно!')
+        points['player'] += 1
+    else:
+        print('Ты ошибся, давай!')
+        points['comp'] += 1
+    print(f'счет: {points["player"]} : {points["comp"]}')
+
